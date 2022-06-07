e1e14b53b185304efac76fcd75e5dd9e329464ec
Zac Ong <zac.ong@ibm.com>
2022-06-07 13:27:39 +1000

    test config cors 1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://with-cors-3.vercel.app',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
